68cb17485c0b1dba99c3a460f23f09e46ac3eafa
Karthik35k <karthik3vk@gmail.com>
2025-09-09 12:26:12 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename="/ecommerce">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,22 +4,22 @@ const Home = () => {
   return (
     <div className="grid-layout">
       <div className="product">
-        <img src="/rose.jpg" alt="Product 1" />
+        <img src="./rose.jpg" alt="Product 1" />
         <h4>Product 1</h4>
         <p>$10.00</p>
       </div>
       <div className="product">
-        <img src="/rose.jpg" alt="Product 2" />
+        <img src="./rose.jpg" alt="Product 2" />
         <h4>Product 2</h4>
         <p>$10.00</p>
       </div>
       <div className="product">
-        <img src="/rose.jpg" alt="Product 3" />
+        <img src="./rose.jpg" alt="Product 3" />
         <h4>Product 3</h4>
         <p>$10.00</p>
       </div>
       <div className="product">
-        <img src="/rose.jpg" alt="Product 4" />
+        <img src="./rose.jpg" alt="Product 4" />
         <h4>Product 4</h4>
         <p>$10.00</p>
       </div>
