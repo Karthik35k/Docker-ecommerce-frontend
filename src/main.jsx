f62aa694830710fedb107293158a09d56149731e
Karthik35k <karthik3vk@gmail.com>
2025-09-18 12:12:37 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { CartProvider } from "./context/components/CartContext.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { CartProvider } from "./context/components/CartContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CartProvider>
-      <App/>
+      <App />
     </CartProvider>
   </StrictMode>
-)
+);
